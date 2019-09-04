a3530904f6621e88d8d83548278081ec3c3148bf
Scott Chamberlain <myrmecocystus@gmail.com>
2019-09-04 10:14:21 -0700

    change link to verb-GET for ... curl options param definition

--- man-roxygen/curl.R
+++ man-roxygen/curl.R
@@ -1,2 +1,2 @@
-#' @param ... curl options passed on to \code{\link[crul]{HttpClient}},
+#' @param ... curl options passed on to \code{\link[crul]{verb-GET}},
 #' see \code{?curl::curl_options} for help on curl options
